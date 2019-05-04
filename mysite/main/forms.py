8b420aad193380beefcb5c8aa6fa69ae42d0892c
Schaeffer Duncan <SchaefferDuncan@gmail.com>
2019-05-04 18:04:00 -0500

    early implementation of printing values from curriculum form

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -2,10 +2,37 @@ from django import forms
 
 
 class CurriculumForm(forms.Form):
-    curriculum_name = forms.CharField()
-    admin_name = forms.CharField()
-    admin_id = forms.IntegerField()
-    min_credits = forms.IntegerField()
-    topic_coverage = forms.CharField()
-    goal_valid_credits = forms.IntegerField()
-    goal_valid = forms.BooleanField()
+    curriculum_name = forms.CharField(
+
+    )
+    admin_name = forms.CharField(
+
+    )
+    admin_id = forms.IntegerField(
+
+    )
+    min_credits = forms.IntegerField(
+
+    )
+    topic_coverage = forms.CharField(
+
+    )
+    goal_valid_credits = forms.IntegerField(
+
+    )
+    goal_valid = forms.BooleanField(
+
+    )
+
+
+    def clean(self):
+        cleaned_data = super(CurriculumForm, self).clean()
+        curriculum_name = cleaned_data.get('curriculum_name')
+        admin_name = cleaned_data.get('admin_name')
+        admin_id = cleaned_data.get('admin_id')
+        min_credits = cleaned_data.get('min_credits')
+        topic_coverage = cleaned_data.get('topic_coverage')
+        goal_valid_credits = cleaned_data.get('goal_valid_credits')
+        goal_valid = cleaned_data.get('goal_valid')
+        if not curriculum_name and not admin_name:
+            raise forms.ValidationError('You have to write something!')
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -16,7 +16,19 @@ def curriculum(request):
     if request.method == 'POST':
         form = forms.CurriculumForm(request.POST)
         if form.is_valid():
-            print('valid form')
+            form.clean()
+            print('Curriculum Name = ' + form['curriculum_name'].value())
+            print('Admin Name = ' + form['admin_name'].value())
+            print('Admin ID = ' + form['admin_id'].value())
+            print('Min Credits = ' + form['min_credits'].value())
+            print('Topic Coverage = ' + form['topic_coverage'].value())
+            print('Goal Valid Credits = ' + form['goal_valid_credits'].value())
+            if form['goal_valid'].value():
+                print('Goat Valid = true')
+            else:
+                print('Goat Valid = true')
+            print('FORM PASSED')
+            pass
     else:
         form = forms.CurriculumForm()
 
